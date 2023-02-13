79485ab5c4810ba43c79f123e14d134cdf74dda8
DiviZition <vitya.popo0201@gmail.com>
2023-02-13 15:51:37 +0300

    Buff Bottles fix;

--- Assets/Scripts/ItemsScripts/ItemShaker.cs
+++ Assets/Scripts/ItemsScripts/ItemShaker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db29951bc2bde91d5cd8c582148df04e9f72e4c07dc12cbc4558abad7afa386a
-size 791
+oid sha256:aeddb825d0775860a7ba83bb5535a086d00e7145b528a0dc937cd0d43cf5204e
+size 811
