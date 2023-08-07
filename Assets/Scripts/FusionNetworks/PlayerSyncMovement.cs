0c660dc72c21cea50d79373bfda2620be1ec7ce2
DiviZition <vitya.popo0201@gmail.com>
2023-08-07 20:19:45 +0300

    little fix;

--- Assets/Scripts/FusionNetworks/PlayerSyncMovement.cs
+++ Assets/Scripts/FusionNetworks/PlayerSyncMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34b1e8fff3dade79daa312300a745454070a07c6690ecca7b73948b4c7a2114e
-size 1671
+oid sha256:79785d8618546f13b09cad35da3e689a8a64506407a5c3c6f692f14aed40053f
+size 477
