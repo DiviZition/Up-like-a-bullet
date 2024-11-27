2d2aef5947dfffc424d0bb697fc3d69de1b84eb2
DiviZition <vitya.popo0201@gmail.com>
2024-11-27 22:59:09 +0300

    Russian localization added;

--- Assets/Scripts/UIScripts/InGameMenus/LegendShowing.cs
+++ Assets/Scripts/UIScripts/InGameMenus/LegendShowing.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9917fa71e0b266dc1198824c832805aeb579975bb24b6359c816b5676694aa6f
-size 5610
+oid sha256:0a0add1771758793a19f9d9c6a6d2bfd37d78b7ef9fbe7b5a60513c05b621d1c
+size 5300
